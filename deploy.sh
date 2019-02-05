b20f8fc24448eea9c92d9ebf294ca86c5ea40d0e
aben20807 <aben20807@gmail.com>
2019-02-05 10:25:14 +0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+while true; do
+    read -p "Push? [y/n]" yn
+    case $yn in
+        [Yy]* ) git push origin master; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Come Back up to the Project Root
+cd ..
