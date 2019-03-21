200477599fffb3d34eab72ed21632d32cfec0433
aben20807 <aben20807@gmail.com>
2019-03-21 22:17:08 +0800

    Create new post automaticlly when using hued

--- hued.sh
+++ hued.sh
@@ -3,6 +3,7 @@
 prefix="content/posts/" # the directory from where to start
 
 RED="\033[0;31m"
+GREEN="\033[0;32m"
 NC="\033[0m"
 
 _hued () {
@@ -29,9 +30,14 @@ hued () {
         echo -e "${RED}error: \"${prefix}\" dir not found${NC}"
         return 1
     fi
+    filename=$@
     # Add prefix directory name
     # Ref: https://stackoverflow.com/a/38558776/6734174
     set -- "${@/#/${prefix}}"
+    if [ ! -f $@ ]; then
+        echo -e "${GREEN}hugo new posts/${filename}${NC}"
+        hugo new "posts/${filename}"
+    fi
     if [[ -n $@ ]]; then
         vim "$@"
     fi
